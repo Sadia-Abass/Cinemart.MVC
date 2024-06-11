e4de10b956924d00a95b89c23a69ce767b9bec6d
Sadia Abass <sadia.abass@outlook.com>
2024-06-11 17:40:33 +0100

    Added DbContext to the project

---
+++ Cinemart.MVC.Cli/Cinemart/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Cinemart.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinemart.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Film> Films { get; set; }
+        public DbSet<FilmShowing> FilmShowing { get; set; }
+        public DbSet<TicketSale> TicketSale { get; set; }
+
+    }
+}
--- Cinemart.MVC.Cli/Cinemart/Models/ApplicationUser.cs
+++ Cinemart.MVC.Cli/Cinemart/Models/ApplicationUser.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Cinemart.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<Guid>
     {
     
         [StringLength(100)]
