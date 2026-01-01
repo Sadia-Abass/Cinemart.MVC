cb4a7d395853bb594309abe037b6515ed48b5a28
Sadia Abass <sadia.abass@outlook.com>
2026-01-01 23:39:08 +0000

    Rename account controller.

---
+++ Cinemart.MVC.Cli/Cinemart/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Cinemart.Data;
+using Cinemart.Models;
+using Cinemart.Services.Implementations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinemart.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly FileUploaderService _fileUploaderService;
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public AccountController(FileUploaderService fileUploaderService, ApplicationDbContext applicationDbContext, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _fileUploaderService = fileUploaderService;
+            _applicationDbContext = applicationDbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
