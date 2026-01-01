c8def64e6dfa9cc19032a820623e8914c0ab863e
Sadia Abass <sadia.abass@outlook.com>
2026-01-01 22:48:51 +0000

    Add fileuploader interface.

---
+++ Cinemart.MVC.Cli/Cinemart/Services/interfaces/IFileUploaderService.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace Cinemart.Services.Interfaces
+{
+    public interface IFileUploaderService
+    {
+        Task<ImageUploadResult> AddFileAsync(IFormFile file);
+        Task<string> DeleteFile(string  publicId);
+    }
+}
