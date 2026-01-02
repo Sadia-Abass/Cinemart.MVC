fb6cf2439fb47c1e2e94802220aff457fac409f3
Sadia Abass <sadia.abass@outlook.com>
2026-01-02 03:36:54 +0000

    Add a post method for register in the controller.

--- Cinemart.MVC.Cli/Cinemart/Controllers/AccountController.cs
+++ Cinemart.MVC.Cli/Cinemart/Controllers/AccountController.cs
@@ -30,10 +30,37 @@ namespace Cinemart.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Register(string returnUrl = null)
+        public async Task<IActionResult> Register(string? returnUrl = null)
         {
             var registerViewModel = new RegisterViewModel();
             registerViewModel.ReturnUrl = returnUrl;
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel, string? returnUrl = null)
+        {
+            registerViewModel.ReturnUrl = returnUrl;
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    Firstname = registerViewModel.Firstname,
+                    Lastname = registerViewModel.Lastname,
+                    UserName = registerViewModel.Email,
+                    Email = registerViewModel.Email,
+                    DOB = registerViewModel.DOB,
+                    imageUrl = registerViewModel.ReturnUrl
+                };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded) 
+                { 
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                ModelState.AddModelError("Password", "User could not be created. Password not unique enough.");
+            }
             return View(registerViewModel);
         }
     }
