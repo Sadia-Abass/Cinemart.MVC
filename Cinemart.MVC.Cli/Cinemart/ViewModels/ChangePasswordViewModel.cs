09748b91e05f750733895582cccb4d5869a2b4fd
Sadia Abass <sadia.abass@outlook.com>
2026-01-05 20:56:18 +0000

    Add change password view model class.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinemart.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "New Password is required.")]
+        [StringLength(34, ErrorMessage = "The {0} must be at least {1} characters long.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Confirm New Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = "The New Password and confirm password entered do not match.")]
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
