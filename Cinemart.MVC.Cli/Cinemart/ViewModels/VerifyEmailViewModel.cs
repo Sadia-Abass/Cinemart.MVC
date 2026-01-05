f0b26a94c811ce53e69a81ec04ce43a3545442f0
Sadia Abass <sadia.abass@outlook.com>
2026-01-05 20:56:50 +0000

    Add verify email view model class.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/VerifyEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinemart.ViewModels
+{
+    public class VerifyEmailViewModel
+    {
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
