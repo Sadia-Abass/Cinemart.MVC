f1ced553ef64c2408e5d33c8a1a38769485b9783
Sadia Abass <sadia.abass@outlook.com>
2026-01-27 12:28:14 +0000

    Add edit user detail view model.

---
+++ Cinemart.MVC.Cli/Cinemart/Models/EditUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinemart.Models
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+        public string imageUrl { get; set; } = string.Empty;
+    }
+}
