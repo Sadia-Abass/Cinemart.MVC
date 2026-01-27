1bb31c63eff251afb69c35a44af18de7a9e51aa6
Sadia Abass <sadia.abass@outlook.com>
2026-01-27 14:56:09 +0000

    Add user roles view model class with required properties.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinemart.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public List<UserRoleItem> Roles { get; set; } = new List<UserRoleItem>();
+    }
+}
