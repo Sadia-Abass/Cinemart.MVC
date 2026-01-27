40ca8233459a925c77fc31a4a16251d4cb3afea8
Sadia Abass <sadia.abass@outlook.com>
2026-01-27 18:44:47 +0000

    Add create role view model class with required entity.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinemart.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Role Description")]
+        public string Discription { get; set; } = string.Empty;
+    }
+}
