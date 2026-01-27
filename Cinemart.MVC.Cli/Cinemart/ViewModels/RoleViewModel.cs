c2dace2213dd3d63e3f80c49ea95a443742982e3
Sadia Abass <sadia.abass@outlook.com>
2026-01-27 18:45:24 +0000

    Add role view model class and required properties.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/RoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinemart.ViewModels
+{
+    public class RoleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Role Description")]
+        public string Discription { get; set; } = string.Empty;
+
+        [Display(Name = "User Count")]
+        public string UserCount { get; set; } = string.Empty;
+    }
+}
