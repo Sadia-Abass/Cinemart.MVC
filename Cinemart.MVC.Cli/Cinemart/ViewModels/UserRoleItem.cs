e7a882ea215688aaf1b604621fc231c4ce30adc3
Sadia Abass <sadia.abass@outlook.com>
2026-01-27 14:55:32 +0000

    Add user role item class.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/UserRoleItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Cinemart.ViewModels
+{
+    public class UserRoleItem
+    {
+        public string RoleName { get; set; } = string.Empty;
+        public bool IsSelected { get; set; }
+    }
+}
