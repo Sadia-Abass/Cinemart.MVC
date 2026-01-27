be390728f0d48ffc7a8c9958735f7e1f4dad05ae
Sadia Abass <sadia.abass@outlook.com>
2026-01-27 19:14:48 +0000

    Add user repository interface.

---
+++ Cinemart.MVC.Cli/Cinemart/Repositories/interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cinemart.ViewModels;
+
+namespace Cinemart.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<UserViewModel>> GetAll();
+        Task<UserViewModel> GetUserByIdAsync(int id);
+        Task<CreateUserViewModel> CreateUserAsync(CreateUserViewModel createUserViewModel);
+        Task<EditUserViewModel> UpdateUserAsync(EditUserViewModel editUserViewModel);
+        Task DeleteUserAsync(int id);
+    }
+}
