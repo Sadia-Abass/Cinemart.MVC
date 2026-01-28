9b673cbc49642d4c0176f93bdc175b7dd0681447
Sadia Abass <sadia.abass@outlook.com>
2026-01-28 23:53:40 +0000

    Add createrolemodelview class with required properties.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/Roles/CreateRoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinemart.ViewModels.Roles
+{
+    public class CreateRoleViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Role Description")]
+        public string Discription { get; set; } = string.Empty;
+
+        [Display(Name = "Active?")]
+        public bool IsActive { get; set; } = true;
+    }
+}
