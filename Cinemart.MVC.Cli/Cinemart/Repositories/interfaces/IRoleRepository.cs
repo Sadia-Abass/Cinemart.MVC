87137c87234ab6781887aa73fa8f462d02a874d9
Sadia Abass <sadia.abass@outlook.com>
2026-01-28 23:52:56 +0000

    Add roles interface.

---
+++ Cinemart.MVC.Cli/Cinemart/Repositories/interfaces/IRoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Cinemart.ViewModels;
+using Cinemart.ViewModels.Roles;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cinemart.Repositories.interfaces
+{
+    public interface IRoleRepository
+    {
+        Task<PagedResult<RoleViewModel>> GetAllRolesAsync(RolefilterViewModel rolefilterViewModel);
+        Task<(IdentityResult Result, Guid? RoleId)> CreateRoleAsync(CreateRoleViewModel createRoleViewModel);
+        Task<EditRoleViewModel> GetRoleForEditAsync(Guid roleId);
+        Task<IdentityResult> EditRoleAsync(EditRoleViewModel editRoleViewModel);
+        Task<IdentityResult> DeleteRoleAsync(Guid roleId);
+        Task<RoleDetailsViewModel?> GetRoleDetailsAsync(Guid roleId, int pageNumber, int pageSize);
+    }
+}
