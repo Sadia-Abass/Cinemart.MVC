eb28e3d42e9afb0f50eb230eeaedc1a8f5fc43ea
Sadia Abass <sadia.abass@outlook.com>
2026-01-28 23:54:28 +0000

    Add roledetailsviewmodel class with required properties.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/Roles/RoleDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cinemart.ViewModels.Roles
+{
+    public class RoleDetailsViewModel
+    {
+        public Guid Id { get; init; }
+        public string? Name { get; init; }
+        public string? Description { get; init; }
+        public bool IsActive { get; init; }
+        public DateTime? CreatedDate { get; init; }
+        public DateTime ModifiedDate { get; init; }
+        public PagedResult<UserInRoleViewModel> Users { get; init; } = new();
+    }
+}
