d4c0ba938b69217ce853b68ed5203e30a1ba16b5
Sadia Abass <sadia.abass@outlook.com>
2026-01-28 23:55:04 +0000

    Add userInRoleViewModel class with required properties.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/Roles/UserInRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cinemart.ViewModels.Roles
+{
+    public class UserInRoleViewModel
+    {
+        public Guid Id { get; init; }
+        public string? Email { get; init; }
+        public string? Firstname { get; init; } 
+        public string? Lastname { get; init; }
+        public bool IsActive { get; init; }
+    }
+}
