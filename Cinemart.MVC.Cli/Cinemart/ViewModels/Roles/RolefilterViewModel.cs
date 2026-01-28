dd190af31a6bcf332d3dc6fe62bcb3d17848f753
Sadia Abass <sadia.abass@outlook.com>
2026-01-28 23:55:50 +0000

    Add rolefilterviewModel class with required properties.

---
+++ Cinemart.MVC.Cli/Cinemart/ViewModels/Roles/RolefilterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinemart.ViewModels.Roles
+{
+    public class RolefilterViewModel
+    {
+        public string? Search {  get; set; }
+        public bool? IsActive { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}
